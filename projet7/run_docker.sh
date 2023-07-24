65272a6958b9dd4a4ca835cba6889f2b08b0705e
Nordine OURAL <nordine.oural@free.fr>
2023-07-24 10:51:06 +0200

    WIP

--- projet7/run_docker.sh
+++ projet7/run_docker.sh
@@ -6,5 +6,5 @@ fi
 
 mkdir -p /home/nordine/Projets/DataScience/DataScience_Training/.cache-projet7
 
-docker run --rm -v /home/nordine/Projets/DataScience/DataScience_Training/projet7:/home/jupyter/notebooks -v /home/nordine/Projets/DataScience/DataScience_Training/.cache-projet7:/home/jupyter/.cache  --network host --gpus all myjupyterlab_p7
+docker run --rm -v /home/nordine/Projets/DataScience/DataScience_Training/projet7:/home/jupyter/notebooks -v /home/nordine/Projets/DataScience/DS_P7_API:/home/jupyter/DS_P7_API -v /home/nordine/Projets/DataScience/DataScience_Training/.cache-projet7:/home/jupyter/.cache  --network host --gpus all myjupyterlab_p7
 
